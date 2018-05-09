2d7bb8f787cdf3e20afd875e719e6b12ae4491d4
Abhishek B. Raut <abhishek.raut1707@gmail.com>
2018-05-09 18:18:07 +0530

    Delete keys.js

--- keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  clientID: '1037385611457-jkb6nkrfupqgjmnk0ga0d1nhh1om54b5.apps.googleusercontent.com',
-  clientSecret: 'JcumvNtN_WhFNzuJAG9UwR0w',
-  callbackURL: 'http://localhost:3000/auth/google/callback'
-}
